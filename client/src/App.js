a9ff9e8528a084de0a66e7907b48065a96404df0
Shawn <shawntennity@gmail.com>
2020-01-18 16:00:55 -0500

    Updated package json to address linting issue.  Created Register form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import Landing from './components/Landing'
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Login from './components/auth/Login';   
+import Register from './components/auth/Register';
 import './App.css';
 
-const  App = () => 
-  <Fragment>
-    <Navbar/>
-    <Landing/>
-  </Fragment>
+const App = () => {
+  return (
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
+  );
+}
 
 export default App;
